564606529ee77d3016264b138048e243a38a26d9
jaydewhyte1992 <135990890+jaydewhyte1992@users.noreply.github.com>
2024-03-29 19:01:32 +1100

    added in Timezones

--- src/script.js
+++ src/script.js
@@ -1,20 +1,41 @@
-var clock = [
-  "&#128347;",
-  "&#128336;",
-  "&#128337;",
-  "&#128338;",
-  "&#128339;",
-  "&#128340;",
-  "&#128341;",
-  "&#128342;",
-  "&#128343;",
-  "&#128344;",
-  "&#128345;",
-  "&#128346;",
-];
+function updateTime() {
+  //melbourne
+  let melbourneElement = document.querySelector("#melbourne");
+  let melbourneDateElement = melbourneElement.querySelector(".date");
+  let melbourneTimeElement = melbourneElement.querySelector(".time");
+  let melbourneTime = moment();
+  melbourneDateElement.innerHTML = melbourneTime.format("MMMM Do YYYY");
+  melbourneTimeElement.innerHTML = melbourneTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
-var counter = 0;
-setInterval(function () {
-  document.getElementById("clock").innerHTML = clock[counter % clock.length];
-  counter++;
-}, 100);
+  //tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //copenhagen
+  let copenhagenElement = document.querySelector("#copenhagen");
+  let copenhagenDateElement = copenhagenElement.querySelector(".date");
+  let copenhagenTimeElement = copenhagenElement.querySelector(".time");
+  let copenhagenTime = moment().tz("Europe/Copenhagen");
+  copenhagenDateElement.innerHTML = copenhagenTime.format("MMMM Do YYYY");
+  copenhagenTimeElement.innerHTML = copenhagenTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //lisbon
+  let lisbonElement = document.querySelector("#lisbon");
+  let lisbonDateElement = lisbonElement.querySelector(".date");
+  let lisbonTimeElement = lisbonElement.querySelector(".time");
+  let lisbonTime = moment().tz("Europe/Dublin");
+  lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+  lisbonTimeElement.innerHTML = lisbonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
